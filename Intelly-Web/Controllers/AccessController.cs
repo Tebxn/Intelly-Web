9b05b8d21fdc0813c87e04c164a74376217b2d8d
Esteban Sanchez <easanrui@gmail.com>
2023-08-19 23:34:59 -0600

    Se agregan varias vistas de la capa de presentacion

---
+++ Intelly-Web/Controllers/AccessController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Intelly_Web.Controllers
+{
+    public class AccessController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public AccessController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult NewUser()
+        {
+            return View();
+        }
+    }
+}
---
+++ Intelly-Web/Controllers/TargetAnalysisController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Intelly_Web.Controllers
+{
+    public class TargetAnalysisController : Controller
+    {
+        public IActionResult BuyerPersonaMain()
+        {
+            return View();
+        }
+    }
+}
