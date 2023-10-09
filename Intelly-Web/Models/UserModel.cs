6cdd0741b1997e25d3171bf667c0a591147de7ef
Gaboo-H <gaboherrera200020@gmail.com>
2023-10-09 17:58:00 -0600

    Update UserModel.cs

--- Intelly-Web/Models/UserModel.cs
+++ Intelly-Web/Models/UserModel.cs
@@ -1,5 +1,9 @@
-﻿using Intelly_Web.Entities;
+﻿﻿using Intelly_Web.Entities;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 using Intelly_Web.Services.Interfaces;
+
 
 namespace Intelly_Web.Models
 {
@@ -8,72 +12,75 @@ namespace Intelly_Web.Models
         private readonly HttpClient _httpClient;
         private readonly IConfiguration _configuration;
         private String _urlApi;
-        private IEmailService _emailService;
+ 	private IEmailService _emailService;
 
         public UserModel(HttpClient httpClient, IConfiguration configuration, IEmailService emailService)
         {
             _httpClient = httpClient;
             _configuration = configuration;
             _urlApi = _configuration.GetSection("Llaves:urlApi").Value;
-            _emailService = emailService;
+	    _emailService = emailService;
+
         }
 
-        public int AddUser(UserEntity entity)//REGISTRO
+        int IUserModel.AddUser(UserEnt entity)//REGISTRO
         {
             string url = _urlApi + "/api/Authentication/RegisterAccount";
             JsonContent obj = JsonContent.Create(entity);
             var resp = _httpClient.PostAsync(url, obj).Result;
             return resp.Content.ReadFromJsonAsync<int>().Result;
         }
-        public List<UserEntity> GetAllUsers()
+
+        /* public List<UserEntity> GetAllUsers()
+       {
+           string url = "/api/Usuario/GetAllUsers";
+           var resp = _httpClient.GetAsync(_urlApi + url).Result;
+
+           if (resp.IsSuccessStatusCode)
+           {
+               var userEntAnswer = resp.Content.ReadFromJsonAsync<UserEntAnswer>().Result;
+
+               if (userEntAnswer != null && userEntAnswer.Objects != null)
+               {
+                   return userEntAnswer.Objects;
+               }
+           }
+
+           return new List<UserEntity>(); // Return an empty list in case of an error
+       }
+
+          }*/
+
+
+        public async Task<List<UserEnt>> GetAllUsers()
         {
-            string url = "/api/Usuario/GetAllUsers";
-            var resp = _httpClient.GetAsync(_urlApi + url).Result;
+            string url = _urlApi + "/api/Users/GetAllUsers";
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
 
-            if (resp.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode)
             {
-                var userEntAnswer = resp.Content.ReadFromJsonAsync<UserEntAnswer>().Result;
-
-                if (userEntAnswer != null && userEntAnswer.Objects != null)
-                {
-                    return userEntAnswer.Objects;
-                }
+                string json = await response.Content.ReadAsStringAsync();
+                List<UserEnt> users = JsonConvert.DeserializeObject<List<UserEnt>>(json);
+                return users;
             }
 
-            return new List<UserEntity>(); // Return an empty list in case of an error
+            throw new Exception("Error al obtener usuarios del API.");
         }
 
-
-
-        /*
-
-        public void EditUser(UserEntity entity)
-
-        {
-
-        }
-
-        public void DeleteUser(UserEntity entity)
-
-        {
-
-        }*/
-        public UserEntity? Login(UserEntity entity)
+        UserEnt? IUserModel.Login(UserEnt entity)
         {
             string url = _urlApi + "/api/Authentication/Login";
             JsonContent obj = JsonContent.Create(entity);
             var resp = _httpClient.PostAsync(url, obj).Result;
 
             if (resp.IsSuccessStatusCode)
-                return resp.Content.ReadFromJsonAsync<UserEntity>().Result;
+                return resp.Content.ReadFromJsonAsync<UserEnt>().Result;
             else
                 return null;
         }
-
-        public void SendEmail(string email)
+	public void SendEmail(string email)
         {
             _emailService.SendEmail(email);
         }
-
     }
 }
