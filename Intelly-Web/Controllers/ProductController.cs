1dae2c3e3de6f5ee9aa90e2d32293dc64b10f92a
Sharon-MN <sharonmn071@gmail.com>
2023-12-03 01:30:56 -0600

    Get all products and add products working

---
+++ Intelly-Web/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using Intelly_Web.Entities;
+using Intelly_Web.Interfaces;
+using Intelly_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Intelly_Web.Controllers
+{
+    public class ProductController : Controller 
+    {
+
+        private readonly IProductModel _productModel;
+
+        private readonly ICompanyModel _companyModel;
+
+        private readonly IHttpContextAccessor _HttpContextAccessor;
+
+        public ProductController(IProductModel productModel, ICompanyModel companyModel, IHttpContextAccessor httpContextAccessor)
+        {
+            _productModel = productModel;
+            _companyModel = companyModel;
+            _HttpContextAccessor = httpContextAccessor;
+
+
+        }
+        [HttpGet]
+        [SecurityFilter]
+        public async Task<IActionResult> AddProduct()
+        {
+            try
+            {
+                var companyDropdownData = await _companyModel.GetAllCompanies();
+                ViewBag.ListCompanies = companyDropdownData.Data.Select(company => new SelectListItem
+                {
+                    Value = company.Company_Id.ToString(),
+                    Text = company.Company_Name
+                });
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.MensajePantalla = "Error al cargar los datos";
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [SecurityFilter]
+        public async Task<IActionResult> AddProduct (ProductEnt entity)
+        {
+            var apiResponse = await _productModel.AddProduct(entity);
+
+            if (apiResponse.Success)
+            {
+                return RedirectToAction("GetAllProducts", "Product");
+            }
+            else
+            {
+                ViewBag.MensajePantalla = "No se realizaron cambios";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        [SecurityFilter]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            try
+            {
+                var apiResponse = await _productModel.GetAllProducts();
+                var listProducts = apiResponse.Data.ToList();
+                return View(listProducts);
+            }
+            catch (Exception)
+            {
+                List<ProductEnt> errors = new List<ProductEnt>();
+                return View(errors);
+            }
+        }
+
+
+    }
+}
---
+++ Intelly-Web/Models/ProductModel.cs
@@ -0,0 +1,151 @@
+﻿using Intelly_Web.Entities;
+using Intelly_Web.Interfaces;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace Intelly_Web.Models
+{
+    public class ProductModel : IProductModel
+    {
+
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _HttpContextAccessor;
+        private String _urlApi;
+        private readonly ITools _tools;
+
+        public ProductModel(HttpClient httpClient, IConfiguration configuration, IHttpContextAccessor httpContextAccessor, ITools tools)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+            _HttpContextAccessor = httpContextAccessor;
+            _urlApi = _configuration.GetSection("Llaves:urlApi").Value;
+            _tools = tools;
+
+        }
+
+        public async Task<ApiResponse<ProductEnt>> AddProduct(ProductEnt entity)
+        {
+            ApiResponse<ProductEnt> response = new ApiResponse<ProductEnt>();
+
+            try
+            {
+                string url = _urlApi + "/api/Product/AddProduct";
+                JsonContent obj = JsonContent.Create(entity);
+                var httpResponse = await _httpClient.PostAsync(url, obj);
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    response.Success = true;
+                    response.Data = await httpResponse.Content.ReadFromJsonAsync<ProductEnt>();
+                    return response;
+                }
+                else
+                {
+                    response.ErrorMessage = "Error Agregar Compañía. Verifique los datos.";
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Error inesperado al agregar compañía: " + ex.Message;
+                return response;
+            }
+        }
+
+        public async Task<ApiResponse<List<ProductEnt>>> GetAllProducts()
+        {
+            ApiResponse<List<ProductEnt>> response = new ApiResponse<List<ProductEnt>>();
+            try
+            {
+                string url = _urlApi + "/api/Product/GetAllProducts";
+                string token = _HttpContextAccessor.HttpContext.Session.GetString("UserToken");
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage httpResponse = await _httpClient.GetAsync(url);
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    string json = await httpResponse.Content.ReadAsStringAsync();
+                    response = JsonConvert.DeserializeObject<ApiResponse<List<ProductEnt>>>(json);
+                    return response;
+                }
+
+                response.ErrorMessage = "Error al obtener productos del API.";
+                response.Code = (int)httpResponse.StatusCode;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Unexpected Error: " + ex.Message;
+                response.Code = 500;
+                return response;
+            }
+        }
+
+        public async Task<ApiResponse<ProductEnt>> GetSpecificProduct(int ProductId)
+        {
+            // Implementa la lógica para obtener una empresa en específico
+            ApiResponse<ProductEnt> response = new ApiResponse<ProductEnt>();
+            try
+            {
+                string url = $"{_urlApi}/api/Product/GetSpecificProduct/{ProductId}";
+                HttpResponseMessage httpResponse = await _httpClient.GetAsync(url);
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    string json = await httpResponse.Content.ReadAsStringAsync();
+                    response = JsonConvert.DeserializeObject<ApiResponse<ProductEnt>>(json);
+                    return response;
+                }
+
+                response.ErrorMessage = "Error al obtener el usuario del API.";
+                response.Code = (int)httpResponse.StatusCode;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Unexpected Error: " + ex.Message;
+                response.Code = 500;
+                return response;
+            }
+        }
+
+        public async Task<ApiResponse<ProductEnt>> EditSpecificProduct(ProductEnt entity)
+        {
+            ApiResponse<ProductEnt> response = new ApiResponse<ProductEnt>();
+
+            try
+            {
+                string url = $"{_urlApi}/api/Product/EditSpecificProduct";
+                JsonContent obj = JsonContent.Create(entity);
+
+                var httpResponse = await _httpClient.PutAsync(url, obj);
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    response.Success = true;
+                    response.Data = await httpResponse.Content.ReadFromJsonAsync<ProductEnt>();
+                }
+                else if (httpResponse.StatusCode == HttpStatusCode.NotFound)
+                {
+                    response.ErrorMessage = "User not found";
+                    response.Code = 404;
+                }
+                else
+                {
+                    response.ErrorMessage = "Unexpected Error: " + httpResponse.ReasonPhrase;
+                    response.Code = (int)httpResponse.StatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = "Unexpected Error: " + ex.Message;
+                response.Code = 500;
+            }
+
+            return response;
+        }
+    }
+}
